49787bdaf592c0ed0262635ccf12c53098f6188a
Min-Yan Hsieh <78300584+ax0080@users.noreply.github.com>
2024-09-02 00:46:02 +0800

    Add files via upload

--- _layout.tsx
+++ _layout.tsx
@@ -3,8 +3,17 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      {/* 主頁的標題修改為 "Main Page" */}
+      <Stack.Screen name="index" options={{ title: 'Main Page' }} />
+      {/* 其他页面 */}
+      <Stack.Screen name="Index1Page" options={{ title: 'Index 1 頁面' }} />
+      <Stack.Screen name="Index2Page" options={{ title: 'Index 2 頁面' }} />
+      <Stack.Screen name="Index3Page" options={{ title: 'Index 3 頁面' }} />
+      <Stack.Screen name="Index4Page" options={{ title: 'Index 4 頁面' }} />
+      <Stack.Screen name="Index5Page" options={{ title: 'Index 5 頁面' }} />
+      <Stack.Screen name="Index6Page" options={{ title: 'Index 6 頁面' }} />
+      <Stack.Screen name="Index7Page" options={{ title: 'Index 7 頁面' }} />
     </Stack>
-
   );
 }
+
